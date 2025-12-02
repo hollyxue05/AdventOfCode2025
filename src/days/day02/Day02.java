b4593c666b26c601471b1776e9e1a55c9208f182
Holly <hollyxue05@gmail.com>
2025-12-02 11:05:50 -0500

    day two part one

--- src/days/day02/Day02.java
+++ src/days/day02/Day02.java
@@ -5,9 +5,29 @@ import java.util.ArrayList;
 
 public class Day02 {
 
-    public static int partOne(ArrayList<String> list) {
-        int sum = 0;
+    public static long partOne(ArrayList<String> list) {
+        long sum = 0;
 
+        // looping through all the IDs
+        for (String s : list) {
+            long start = Long.parseLong(s.substring(0, s.indexOf('-')));
+            long end = Long.parseLong(s.substring(s.indexOf('-') + 1));
+            int maxLen = s.substring(s.indexOf('-') + 1).length(); 
+            System.out.println("start: "+ start + ", end: " + end); 
+
+            // looping through the whole range
+            for (long i = start; i <= end; i++) {
+                String first = (String.valueOf(i)).substring(0, (maxLen / 2));
+                String second = (String.valueOf(i)).substring((maxLen / 2));
+                // System.out.println("first = "+first+", second = "+second);
+
+                // check if they are invalid aka repeating digits
+                if(first.equals(second)) {
+                    sum += i; 
+                    System.out.println("added "+i);
+                }                
+            }   
+        }
 
         return sum; 
     }
